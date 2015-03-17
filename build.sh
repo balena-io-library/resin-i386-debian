3adb6e880954a7dc484999123b61c39dca4ed32c
James <james@resin.io>
2015-03-17 02:27:22 +0000

    fix latest tag and install sudo

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -o errexit
 SUITES='wheezy jessie'
 MIRROR='http://ftp.uk.debian.org/debian/'
 REPO='resin/i386-debian'
+LATEST='jessie'
 
 for suite in $SUITES; do
 	dir=$(mktemp --tmpdir=/var/tmp -d)
@@ -14,8 +15,11 @@ for suite in $SUITES; do
 	cp qemu-arm-static $dir/rootfs/usr/bin
 	chmod +x $dir/rootfs/usr/bin/qemu-arm-static
 	
-	./mkimage.sh -t $REPO:$suite --dir=$dir debootstrap --variant=minbase --arch=i386 $suite $MIRROR
+	./mkimage.sh -t $REPO:$suite --dir=$dir debootstrap --variant=minbase --arch=i386 --include=sudo $suite $MIRROR
 	rm -rf $dir
 	
 	docker tag -f $REPO:$suite $REPO:$suite-$date
+	if [ $LATEST == $suite ]; then
+		docker tag -f $REPO:$suite $REPO:latest
+	fi
 done
