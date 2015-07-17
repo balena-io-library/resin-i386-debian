e7ca4a889a5b3619d17b21f76d92b7552fac8c15
James <james@resin.io>
2015-07-17 19:26:20 +0700

    Apply RES-623: set LC_ALL=C.UTF-8 Apply RES-559: export image info to s3

--- automation/jenkins-build.sh
+++ automation/jenkins-build.sh
@@ -2,5 +2,5 @@
 
 # Jenkins build steps
 docker build -t i386-debian-mkimage .
-docker run --privileged -v /var/run/docker.sock:/var/run/docker.sock i386-debian-mkimage
+docker run --privileged -e REGION_NAME=$REGION_NAME -e ACCESS_KEY=$ACCESS_KEY -e SECRET_KEY=$SECRET_KEY -e BUCKET_NAME=$BUCKET_NAME -v /var/run/docker.sock:/var/run/docker.sock i386-debian-mkimage
 docker push resin/i386-debian
--- build.sh
+++ build.sh
@@ -13,6 +13,15 @@ for suite in $SUITES; do
 	
 	./mkimage.sh -t $REPO:$suite --dir=$dir debootstrap --variant=minbase --arch=i386 --include=sudo $suite $MIRROR
 	rm -rf $dir
+
+	docker run --rm $REPO:$suite bash -c 'dpkg-query -l' > $suite
+	cp $suite $suite_$date
+
+	# Upload to S3 (using AWS CLI)
+	printf "$ACCESS_KEY\n$SECRET_KEY\n$REGION_NAME\n\n" | aws configure
+	aws s3 cp $suite s3://$BUCKET_NAME/image_info/i386-debian/$suite/
+	aws s3 cp $suite_$date s3://$BUCKET_NAME/image_info/i386-debian/$suite/
+	rm -f $suite $suite_$date
 	
 	docker tag -f $REPO:$suite $REPO:$suite-$date
 	if [ $LATEST == $suite ]; then
